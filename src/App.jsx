80a1dab2da1aaa92b153a9705fbcaffd4090c429
lealxh <lealxh@gmail.com>
2025-08-31 20:31:23 -0400

    Add documentation to src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import Board from "./components/Board"
+/**
+ * The main application component.
+ * @param {ReactElement} None
+ * @returns {ReactElement} The root React element for the application.
+ * /
+ * function App() {
+ *   return <div className="App"></div>
+ * }
+ */
 
 function App() {
   return <div className="App"></div>
